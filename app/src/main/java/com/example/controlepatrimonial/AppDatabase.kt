3855d48154748c6e1364282bef97fdbf56a81b57
Alvaro Ragazzi <95315441+alvaroragazzi@users.noreply.github.com>
2022-06-04 17:51:52 -0300

    Update AppDatabase.kt

--- app/src/main/java/com/example/controlepatrimonial/AppDatabase.kt
+++ app/src/main/java/com/example/controlepatrimonial/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.controlepatrimonial
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(Equipamento::class), version = 3)
+@Database(entities = arrayOf(Equipamento::class), version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun dao(): Dao
 }
